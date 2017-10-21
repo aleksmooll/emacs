297f4f818985635bc042aa7cced9bc0e998bf732
Aleks Mulyarov <aleksmooll@gmail.com>
2017-10-21 15:26:56 +0700

    ''

--- core/emacs-keybinding.el
+++ core/emacs-keybinding.el
@@ -12,5 +12,76 @@
 (global-set-key (kbd "M-i") 'previous-line)
 (global-set-key (kbd "M-k") 'next-line)
 
+;; Move by word
+(global-set-key (kbd "M-u") 'backward-word)
+(global-set-key (kbd "M-o") 'forward-word) ; was (prefix)
+
+;; Move by paragraph
+(global-set-key (kbd "M-U") 'backward-paragraph)
+(global-set-key (kbd "M-O") 'forward-paragraph)
+
+;; Move to beginning/ending of line
+(global-set-key (kbd "M-h") 'move-beginning-of-line)
+(global-set-key (kbd "M-H") 'move-end-of-line)
+
+;; Move by screen (page up/down)
+(global-set-key (kbd "M-I") 'scroll-down)
+(global-set-key (kbd "M-K") 'scroll-up)
+
+;; Move to beginning/ending of file
+(global-set-key (kbd "M-J") 'beginning-of-buffer)
+(global-set-key (kbd "M-L") 'end-of-buffer)
+
+;; isearch
+(global-set-key (kbd "M-;") 'isearch-forward)
+(global-set-key (kbd "M-:") 'isearch-backward)
+
+;;; MAJOR EDITING COMMANDS
+
+;; Delete previous/next char.
+(global-set-key (kbd "M-d") 'delete-backward-char)
+(global-set-key (kbd "M-f") 'delete-char)
+
+; Delete previous/next word.
+(global-set-key (kbd "M-e") 'backward-kill-word)
+(global-set-key (kbd "M-r") 'kill-word)
+
+; Copy Cut Paste, Paste previous
+(global-set-key (kbd "M-x") 'kill-region)
+(global-set-key (kbd "M-c") 'kill-ring-save)
+(global-set-key (kbd "M-v") 'yank)
+(global-set-key (kbd "M-V") 'yank-pop)
+(global-set-key (kbd "M-C") 'copy-all)
+(global-set-key (kbd "M-X") 'cut-all)
+
+;; undo and redo
+(global-set-key (kbd "M-Z") 'redo)
+(global-set-key (kbd "M-z") 'undo)
+
+; Kill line
+(global-set-key (kbd "M-g") 'kill-line)
+(global-set-key (kbd "M-G") 'kill-line-backward)
+
+;;; EMACS'S SPECIAL COMMANDS
+
+; Mark point.
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+
+(global-set-key (kbd "M-a") 'execute-extended-command)
+(global-set-key (kbd "M-A") 'shell-command)
+
+;;; WINDOW SPLITING
+(global-set-key (kbd "M-s") 'move-cursor-next-pane)
+(global-set-key (kbd "M-S") 'move-cursor-previous-pane)
+
+; keyword completion, because Alt+Tab is used by OS
+(global-set-key (kbd "M-t") 'call-keyword-completion)
+
+(global-set-key (kbd "M-1") 'delete-other-windows)
+(global-set-key (kbd "M-!") 'delete-window)
+
+(global-set-key (kbd "M-2") 'split-window-vertically)
+(global-set-key (kbd "M-@") 'split-window-horizontally)
+
 
 (provide 'emacs-keybinding)
